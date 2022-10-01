2489a91856cf01bcb52f1b3e1b24121fb45b8698
elic4vet <86532060+elic4vet@users.noreply.github.com>
2022-10-01 13:05:09 +0200

    ++

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import { Route, Routes } from 'react-router-dom'
+import Home from './components/Home/index.js'
+import Layout from './components/Layout'
+import About from './components/About/index.js'
+import Contact from './components/Contact/index.js'
+import './App.scss'
+
+function App() {
+  return (
+    <>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="about" element={<About />} />
+          <Route path="contact" element={<Contact />} />
+        </Route>
+      </Routes>
+    </>
+  )
+}
+
+export default App
