77d3bac0702a1962cba367d812622cce97a24880
elic4vet <86532060+elic4vet@users.noreply.github.com>
2022-10-03 19:22:36 +0200

    created the portfolio section

---
+++ src/components/Portfolio/index.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import { useState } from 'react'
+import Loader from 'react-loaders'
+import AnimatedLetters from '../AnimatedLetters'
+import './index.scss'
+
+const Portfolio = () => {
+  const [letterClass, setLetterClass] = useState('text-animate')
+  return (
+    <>
+      <div className="container portfolio-page">
+        <h1 className="page-title">
+          {' '}
+          <AnimatedLetters
+            letterClass={letterClass}
+            strArray={'Portfolio'.split('')}
+            idx={15}
+          />
+        </h1>
+      </div>
+      <Loader type="pacman" />
+    </>
+  )
+}
+export default Portfolio
