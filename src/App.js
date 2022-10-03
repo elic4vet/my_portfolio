fa1abbc27185338e6bb2cd1506b3dc76570605e8
elic4vet <86532060+elic4vet@users.noreply.github.com>
2022-10-03 19:22:13 +0200

    +route portfolio

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Home from './components/Home/index.js'
 import Layout from './components/Layout'
 import About from './components/About/index.js'
 import Contact from './components/Contact/index.js'
+import Portfolio from './components/Portfolio/index.js'
 import './App.scss'
 
 function App() {
@@ -12,7 +13,8 @@ function App() {
         <Route path="/" element={<Layout />}>
           <Route index element={<Home />} />
           <Route path="about" element={<About />} />
-          <Route path="contact" element={<Contact />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/portfolio" element={<Portfolio />} />
         </Route>
       </Routes>
     </>
