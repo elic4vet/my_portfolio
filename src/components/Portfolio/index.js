5b54a1ee6d9bc7c3d0a96ba3024537340ec9024d
elic4vet <86532060+elic4vet@users.noreply.github.com>
2022-10-07 20:34:54 +0200

    Update index.js

--- src/components/Portfolio/index.js
+++ src/components/Portfolio/index.js
@@ -1,11 +1,51 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { useState } from 'react'
 import Loader from 'react-loaders'
 import AnimatedLetters from '../AnimatedLetters'
+import portfolioData from '../../data/portfolio.json'
 import './index.scss'
 
+/*-__-*/
+
 const Portfolio = () => {
-  const [letterClass] = useState('text-animate')
+  const [letterClass, setLetterClass] = useState('text-animate')
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setLetterClass('text-animate-hover')
+    }, 3000)
+
+    return () => {
+      clearTimeout(timer)
+    }
+  })
+
+  const renderPortfolio = (portfolio) => {
+    return (
+      <div className="images-container">
+        {portfolio.map((port, idx) => {
+          return (
+            <div className="image-box" key={idx}>
+              <img
+                src={port.image}
+                className="portfolio-image"
+                alt="portfolio"
+              />
+
+              <div className="content">
+                <p className="title">{port.name}</p>
+                <h4 className="description">{port.description}</h4>
+                <button className="btn" onClick={() => window.open(port.url)}>
+                  View
+                </button>
+              </div>
+            </div>
+          )
+        })}
+      </div>
+    )
+  }
+
   return (
     <>
       <div className="container portfolio-page">
@@ -17,9 +57,11 @@ const Portfolio = () => {
             idx={15}
           />
         </h1>
+        <div> {renderPortfolio(portfolioData.portfolio)} </div>
       </div>
       <Loader type="pacman" />
     </>
   )
 }
+
 export default Portfolio
