9b47ee486ea51c24a142bc209e4a16d6a53b681f
elic4vet <86532060+elic4vet@users.noreply.github.com>
2022-10-16 19:13:28 +0200

    +hamburger-icon

--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -1,4 +1,5 @@
 import './index.scss'
+import { useState } from 'react'
 import logo from '../../assets/images/LOGO_small.png'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import {
@@ -6,71 +7,102 @@ import {
   faHome,
   faSuitcase,
   faUser,
+  faBars,
+  faClose,
 } from '@fortawesome/free-solid-svg-icons'
 import { Link, NavLink } from 'react-router-dom'
 
-const Sidebar = () => (
-  <div className="nav-bar">
-    {' '}
-    <Link className="logo" to="/">
-      <img src={logo} alt="logo" />
-    </Link>
-    <nav>
-      <NavLink exact="true" activeclassname="active" to="/">
-        <FontAwesomeIcon icon={faHome} color="#cb4063" />
-      </NavLink>
+const Sidebar = () => {
+  const [showNav, setShowNav] = useState(false)
 
-      <NavLink
-        exact="true"
-        activeclassname="active"
-        className="about-link"
-        to="/about"
-      >
-        <FontAwesomeIcon icon={faUser} color="#cb4063" />
-      </NavLink>
+  return (
+    <div className="nav-bar">
+      <Link className="logo" to="/" onClick={() => setShowNav(false)}>
+        <img src={logo} alt="logo" />
+      </Link>
 
-      <NavLink
-        exact="true"
-        activeclassname="active"
-        className="portfolio-link"
-        to="/portfolio"
-      >
-        <FontAwesomeIcon icon={faSuitcase} color="#cb4063" />
-      </NavLink>
-
-      <NavLink
-        exact="true"
-        activeclassname="active"
-        className="contact-link"
-        to="/contact"
-      >
-        <FontAwesomeIcon icon={faEnvelope} color="#cb4063" />
-      </NavLink>
-
-      <li>
-        <a
-          target="_blank"
-          rel="noreferrer"
-          className="linkendin-link"
-          href="https://www.linkedin.com/in/eerkekoglou/"
+      <nav className={showNav ? 'mobile-show' : ''}>
+        <NavLink
+          exact="true"
+          activeclassname="active"
+          to="/"
+          onClick={() => setShowNav(false)}
         >
-          <i class="fa fa-linkedin"></i>
-        </a>
-      </li>
+          <FontAwesomeIcon icon={faHome} color="#cb4063" />
+        </NavLink>
 
-      <li>
-        <a
-          target="_blank"
-          rel="noreferrer"
-          className="github-link"
-          href=" https://github.com/elic4vet"
-          to="github"
+        <NavLink
+          exact="true"
+          activeclassname="active"
+          className="about-link"
+          to="/about"
+          onClick={() => setShowNav(false)}
         >
-          <i class="fa fa-github icon"></i>
-        </a>
-      </li>
-    </nav>
-  </div>
-)
+          <FontAwesomeIcon icon={faUser} color="#cb4063" />
+        </NavLink>
+
+        <NavLink
+          exact="true"
+          activeclassname="active"
+          className="portfolio-link"
+          to="/portfolio"
+          onClick={() => setShowNav(false)}
+        >
+          <FontAwesomeIcon icon={faSuitcase} color="#cb4063" />
+        </NavLink>
+
+        <NavLink
+          exact="true"
+          activeclassname="active"
+          className="contact-link"
+          to="/contact"
+          onClick={() => setShowNav(false)}
+        >
+          <FontAwesomeIcon icon={faEnvelope} color="#cb4063" />
+        </NavLink>
+
+        <FontAwesomeIcon
+          onClick={() => setShowNav(false)}
+          icon={faClose}
+          color="#cb4063"
+          size="3x"
+          className="close-icon"
+        />
+      </nav>
+
+      <ul>
+        <li>
+          <a
+            target="_blank"
+            rel="noreferrer"
+            className="linkendin-link"
+            href="https://www.linkedin.com/in/eerkekoglou/"
+          >
+            <i class="fa fa-linkedin" />
+          </a>
+        </li>
+        <li>
+          <a
+            target="_blank"
+            rel="noreferrer"
+            className="github-link"
+            href=" https://github.com/elic4vet"
+            to="github"
+          >
+            <i class="fa fa-github icon"></i>
+          </a>
+        </li>
+      </ul>
+
+      <FontAwesomeIcon
+        onClick={() => setShowNav(true)}
+        icon={faBars}
+        color="#cb4063"
+        size="3x"
+        className="hamburger-icon"
+      />
+    </div>
+  )
+}
 
 export default Sidebar
